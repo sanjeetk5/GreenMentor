0b06670a654096b14a497cec8484b2c17927fda6
sanjeetk5 <112753484+sanjeetk5@users.noreply.github.com>
2024-04-04 15:59:43 +0530

    completed addes

--- server/models/task.model.js
+++ server/models/task.model.js
@@ -10,6 +10,11 @@ const taskSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+
+  completed: {
+    type: Boolean,
+    default: false, // Default value is false indicating the task is not completed initially
+  },
   userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
   createdAt: {
     type: Date,
